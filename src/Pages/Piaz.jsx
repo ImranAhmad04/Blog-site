f0282f35b4999defa05a363b0417a66d15ef2e7d
ImranAhmed <emranmd368@gmail.com>
2023-12-23 00:46:45 -0800

    Updates

--- src/Pages/Piaz.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Piaz() {
-  return (
-    <div>piaz is not avialable</div>
-  )
-}
-
-export default Piaz
--- src/Pages/reactBootstrap.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react' 
-export default function ReactBootstrap() {
-    return (
-        <>
-            <button className='btn btn-primary'> btn </button>
-            <button className='btn btn-warning'> btn warning</button>
-        </>
-    )
-}
